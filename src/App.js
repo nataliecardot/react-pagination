10754913602016a8da49a6e67a39c6f49dcbfda1
Natalie-Cardot <nataliecardot@gmail.com>
2020-02-18 15:00:24 -0800

    Add state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
   return (
     <div className="container">
       <h1>My App</h1>
