f1526760ae1973d932c0088ba9ad119882867dfc
Natalie-Cardot <nataliecardot@gmail.com>
2020-02-17 22:45:38 -0800

    Add Axios and Bootstrap 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return <div></div>;
-}
+const App = () => {
+  return (
+    <div className="container">
+      <h1>My App</h1>
+    </div>
+  );
+};
 
 export default App;
