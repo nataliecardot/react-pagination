eee53a5e3f24f6e0292f13b2d4333bec49116b41
Natalie-Cardot <nataliecardot@gmail.com>
2020-02-18 19:23:46 -0800

    Fetch posts

--- src/App.js
+++ src/App.js
@@ -8,6 +8,22 @@ const App = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage, setPostsPerPage] = useState(10);
 
+  // Make request once component is mounted to DOM
+  // useEffect is similiar to componentDidMount and componentDidUpdate
+  // When this runs it will update the component and it would be a neverending loop. To stop that, an empty array is passed as second argument; state inside effect will retain initial values (if you wanted it to run only when certain values change, those values would be passed in). With that modification, it works like componentDidMount
+  useEffect(() => {
+    // Creating new function because want to use async/await, but usage of async with useEffect not supported
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('http://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  console.log(posts);
   return (
     <div className="container">
       <h1>My App</h1>
